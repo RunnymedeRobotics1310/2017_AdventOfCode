ea25c99e2b1f9679bee4a78d3b4d980624081c4d
richardmcmullin <richard.mcmullin@altairix.com>
2016-11-29 20:00:10 -0500

    Structure for Day 01 Code

---
+++ AdventOfCode/src/day01/rem/Day01.java
@@ -0,0 +1,38 @@
+package day01.rem;
+
+import java.util.Scanner;
+
+/**
+ * This class shows the general structure to the AdventOfCode 
+ * solutions for 2017
+ */
+public class Day01 {
+
+	private Scanner input = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		(new Day01()).solve();
+	}
+
+	/**
+	 * Solve the day's puzzle
+	 */
+	private void solve() {
+		
+		System.out.println("Paste puzzle input here...");
+		
+		while (true) {
+			
+			// Continue reading input lines until a blank line is encountered
+			String s = input.nextLine();
+
+			if (s.trim().isEmpty()) { break; }
+			
+			// Process each input line
+			// ... insert solution code here
+		}
+		
+		// Print the solution
+		System.out.println("Solution");
+	}
+}
